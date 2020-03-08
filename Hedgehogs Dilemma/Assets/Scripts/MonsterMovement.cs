a70e7003f1c7e5e523295fa3d113f2e6c077e1af
César Neves Mosca <Cronokami@hotmail.com>
2020-03-08 02:19:05 -0300

    Movimento do bichão

---
+++ Hedgehogs Dilemma/Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterMovement : MonoBehaviour
+{
+
+	public Camera cam;
+	public NavMeshAgent agent;
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit))
+			{
+				agent.SetDestination(hit.point);
+				Debug.Log(agent.destination);
+			}
+		}
+	}
+}
