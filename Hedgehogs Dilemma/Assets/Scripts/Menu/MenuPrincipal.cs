a010d5adf00083d82b22d7d52ed433ccf6be7b90
César Neves Mosca <Cronokami@hotmail.com>
2020-03-08 16:53:59 -0300

    Iniciando menu

---
+++ Hedgehogs Dilemma/Assets/Scripts/Menu/MenuPrincipal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+	public void StartGame()
+	{
+
+
+	}
+
+	public void Credits()
+	{
+
+
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+
+	}
+}
