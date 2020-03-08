5189763e837edc4c4e1b0209c45638eed7052b0d
César Neves Mosca <Cronokami@hotmail.com>
2020-03-08 14:59:22 -0300

    Os carrinho se mexendo

---
+++ Hedgehogs Dilemma/Assets/Scripts/CarCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCollision : MonoBehaviour
+{
+	private void OnCollisionEnter(Collision collision)
+	{
+		
+
+		if (collision.gameObject.CompareTag("Monstro"))
+		{
+
+
+		}
+
+		else if (collision.gameObject.CompareTag("Car"))
+		{
+			//por enquanto nada acontece feijoada, eles nem se batem na layer, se quiser que se batam, troca as propriedades da layer deles
+
+		}
+	}
+}
---
+++ Hedgehogs Dilemma/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CarMovement : MonoBehaviour
+{
+	public Transform[] targetPoints;
+
+	[HideInInspector]
+	public NavMeshAgent agent;
+
+
+	private void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		agent.SetDestination(targetPoints[Random.Range(0, targetPoints.Length - 1)].position);
+	}
+
+	private void Update()
+	{
+		if (Vector3.Distance(agent.destination, transform.position) <= 5)
+		{
+			agent.SetDestination(targetPoints[Random.Range(0, targetPoints.Length - 1)].position);
+		}
+	}
+}
